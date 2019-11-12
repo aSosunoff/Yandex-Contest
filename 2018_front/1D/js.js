6b9a4dfa96c7765f949b933f5a1853955761347f
asosunoff <al.sosunoff@gmail.com>
2019-11-12 19:52:48 +0300

    fixed 1D

--- 2018_front/1D/js.js
+++ 2018_front/1D/js.js
@@ -26,13 +26,13 @@ function calcFontSize(container, str, min, max) {
     if(!check(min)) return null;
     if(check(max)) return max;
 
-    const getNewSize = (l, r) => Math.floor((r + l) / 2);
+    const getNewSize = (l, r) => Math.round((r + l) / 2);
 
     let l = min;
     let r = max;
     while(l < r - 1){
-        debugger;
         let m = getNewSize(l, r);
+
         if(check(m))
             l = m;
         else
@@ -41,50 +41,6 @@ function calcFontSize(container, str, min, max) {
 
     return l;
 }
-
-/***
- * @param container {Node} ссылка на DOM-node, в которую нужно вписать строку ‘str‘
- * @param str {string} строка, которую необходимо вписать. Максимальная длина равняется 100 символам
- * @param min {number} минимальный размер шрифта (целое число, min >= 1)
- * @param max {number} максимальный размер шрифта (целое число, max >= min >= 1)
- * @return {number | null} искомый размер шрифта (целое число) или null, если текст вписать нельзя
- */
-// function calcFontSize(container, str, min, max) {
-//     const checkFit = size => {
-//         container.style.setProperty('font-size', `${size}px`, 'important');
-//         const {height, width} = container.getBoundingClientRect();
-//         return container.scrollHeight <= height && container.scrollWidth <= width;
-//     };
-//     const getNewSize = (min, max) => Math.round((max + min) / 2);
-//
-//     container.textContent = str;
-//     let newMin = min;
-//     let newMax = max;
-//     let current = getNewSize(min, max);
-//     let newCurrent;
-//     let size = null;
-//
-//     if (checkFit(max)) return max;
-//     if (!checkFit(min)) return null;
-//     if (!str) return null;
-//
-//     while (true){
-//         if (checkFit(current)){
-//             size = current;
-//             newMin = current;
-//             newCurrent = getNewSize(newMin, newMax);
-//             if (current === newCurrent) break;
-//             current = newCurrent;
-//         } else {
-//             newMax = current;
-//             newCurrent = getNewSize(newMin, newMax);
-//             if (current === newCurrent) break;
-//             current = newCurrent;
-//         }
-//     }
-//
-//     return size;
-// }
 
 
 calcFontSize(document.getElementById("container"), "1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111", 10, 100)
