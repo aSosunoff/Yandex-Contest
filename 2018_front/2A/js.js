36fb36c35b90f55edf4d527210f0e0e5919a6cf6
asosunoff <al.sosunoff@gmail.com>
2019-11-23 16:09:11 +0300

    Fix 2A

--- 2018_front/2A/js.js
+++ 2018_front/2A/js.js
@@ -1,16 +1,18 @@
 /** @returns string */
 function solve(inputString){
-    let regexp = new RegExp("^.*ta'(?<name>so|ko|ta|qa|goo)\\s*(?<date>\\d+).*$", "i");
+    let find = Array.from(inputString.matchAll(/ta['’](?<name>so|ko|ta|qa|goo)\s*(?<number>\d+)/ig));
 
-    if(regexp.test(inputString)){
-        let res = regexp.exec(inputString);
-        return `${res.groups.name.toLowerCase()} ${res.groups.date}`;
+    if(find && find.length == 1){
+        let res = find[0];
+
+        return `${res.groups.name} ${res.groups.number}`.toLowerCase();
     }
 
     return '0';
 }
-
+//Ta’gh ta’So 29 jE yin
+//^.*ta['’](?<name>so|ko|ta|qa|goo)\\s*(?<number>\\d+).*$
 setTimeout(() => {
-    console.log(solve('DUN \'Ej QAD Je pAtLh TLhOQ Ta\'ta 494 PuS WoVBe\' SICh HuD,'));
+    console.log(solve('DUN \'Ej QAD Je pAtLh TLhOQ Ta\'ta 23 PuS WoVBe\' SICh HuD,'));
     //console.log(solve('Ta\'gh ta\'so 23 jE yin'));
 }, 500)
