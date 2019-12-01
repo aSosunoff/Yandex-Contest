c0bede87406a564ede6471a9cd102f5b195a72d4
asosunoff <al.sosunoff@gmail.com>
2019-12-02 00:59:18 +0300

    3A is completed

--- 2018_front/3A/js.js
+++ 2018_front/3A/js.js
@@ -2,38 +2,45 @@
 function solve(inputString){
     if(!inputString) return null;
 
-    let arr = inputString.split(/\s*,\s*/);
-
+    let _separator_ = '\\s*,\\s*';
     let _word = '[А-Я][а-я]{2,7}',
     _digit = '[0-9~§]',
     _name = 'Сёб|Рёб|Моб|Сян|Рян|Ман',
     _surname = 'Сё|Рё|Мо|Ся|Ря|Ма|Сю|Рю|Му';
     
-    let _a = `^${_word}(?:[ -]${_word})?$`,
-    _b = `^(?:${_digit}{3})$|^(?:${_digit}{2}-${_digit}{2})$`,
-    _c = `^(?:${_surname}) (?:${_name})$`;
+    let _a = `${_word}(?:[ -]${_word})?`,
+    _b = `(?:${_digit}{3})|(?:${_digit}{2}-${_digit}{2})`,
+    _c = `(?:${_surname}) (?:${_name})`;
 
-    let validAddress = [_a, _a, _a, _b, _c]; 
-    
+    let validLineArr = [
+        {
+            validate: [_a, _a, _a, _b, _c],
+            line: [0, 1, 2, 3, 4]
+        },
+        {
+            validate: [_a, _a, _c],
+            line: [0, 1, null, null, 2]
+        },
+    ];
+
     let resultArray = [];
 
-    let i = 0;
-    arr.forEach(element => {
-        debugger;
-        while(i < 5){
-            let rule = validAddress[i];
-            let data = element.match(rule);
-    
-            if(data){
-                resultArray.push(data[0]);
-                i++;
-                break;
-            }
-            resultArray.push(null);
-            i++;
+    for(let valid of validLineArr){
+        let rule = valid
+            .validate
+            .reduce((r, e) => { 
+                return !r ? `(${e})` : `${r}${_separator_}(${e})` 
+            }, '');
+
+        let data = inputString.match(`^${rule}$`);
+
+        if(data){
+            let preArr = Array.from(data).slice(1);
+            resultArray = valid.line.map(e => { return e == null ? null : preArr[e]; });
+            break;
         }
-    });
-    
+    };
+
     if(resultArray.every(e => e == null))
         return null;
 
@@ -45,8 +52,7 @@ setTimeout(() => {
 }, 500);
 
 let date = [
-    ['Старый, Оскол-Бравый, Му Рян1', ['Старый', 'Оскол-Бравый', null, null, null]],
-
+    ['Старый, Оскол-Бравый, Му Рян1', null],
     ['Старый Оскол, Бравый, Слон, ~2-3§, Му Рян', ['Старый Оскол', 'Бравый', 'Слон', '~2-3§', 'Му Рян']],
     ['Старый Оскол, Бравый, Слон, 12-34, Му Рян', ['Старый Оскол', 'Бравый', 'Слон', '12-34', 'Му Рян']],
     ['Старый Оскол, Бравый, Слон, ~~3, Му Рян', ['Старый Оскол', 'Бравый', 'Слон', '~~3', 'Му Рян']],
@@ -69,7 +75,7 @@ function test(){
             } else {
                 for(let i = 0; i < solving.length; i++){
                     if(solving[i] !== res[i]){
-                        console.log(str, solving[i], res[i]);
+                        console.log(str, '|', solving[i], res[i]);
                         break;
                     }
                 }
