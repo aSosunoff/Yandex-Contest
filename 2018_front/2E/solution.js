f836f2f0349c1b0d6ee71da84d388e2c90d0bb43
aSosunoff <al.sosunoff@gmail.com>
2019-12-04 12:07:24 +0300

    fixed

--- 2018_front/2E/solution.js
+++ 2018_front/2E/solution.js
@@ -3,27 +3,36 @@ function getKeyboard(){
 }
 
 function getNotes(){
-    let buttons = {
-        "C1": 0, "C1 sharp": 1, "D1 flat": 1,
-        "D1": 2, "D1 sharp": 3, "E1 flat": 3,
-        "E1": 4,
-        "F1": 5, "F1 sharp": 6, "G1 flat": 6,
-        "G1": 7, "G1 sharp": 8, "A1 flat": 8,
-        "A1": 9, "A1 sharp": 10, "H1 flat": 10,
-        "H1": 11,
-        "C2": 12, "C2 sharp": 13, "D2 flat": 13,
-        "D2": 14, "D2 sharp": 15, "E2 flat": 15,
-        "E2": 16,
-        "F2": 17, "F2 sharp": 18, "G2 flat": 18,
-        "G2": 19, "G2 sharp": 20, "A2 flat": 20,
-        "A2": 21, "A2 sharp": 22, "H2 flat": 22,
-        "H2": 23
+    let notes = Array.prototype.slice.call(document.querySelectorAll('.game .target .symbol:not(.separator)'));
+
+    let notesGlobal = {
+        1: {
+            'C': { '': 0, '♯': 1, '♭': -1 },
+            'D': { '': 2, '♯': 3, '♭': 1 },
+            'E': { '': 4, '♯': 5, '♭': 3 },
+            'F': { '': 5, '♯': 6, '♭': 4 },
+            'G': { '': 7, '♯': 8, '♭': 6 },
+            'A': { '': 9, '♯': 10, '♭': 8 },
+            'H': { '': 11, '♯': 12, '♭': 10 },
+        },
+        2: {
+            'C': { '': 12, '♯': 13, '♭': 11 },
+            'D': { '': 14, '♯': 15, '♭': 13 },
+            'E': { '': 16, '♯': 17, '♭': 15 },
+            'F': { '': 17, '♯': 18, '♭': 16 },
+            'G': { '': 19, '♯': 20, '♭': 18 },
+            'A': { '': 21, '♯': 22, '♭': 20 },
+            'H': { '': 23, '♯': 24, '♭': 22 },
+        }
+        
     };
 
-    return Array.prototype.slice.call(document.querySelectorAll('.game .target .symbol:not(.separator)'))
+    return notes
         .map(e => {
-            let key = [e.classList[1], e.classList[2]].filter(f => f).join(' ');
-            return buttons[key];
+            let [noteName, octave] = e.classList[1].split('');
+            let sign = getComputedStyle(e, ':before').content;
+            sign = sign.replace(/"/g, '');
+            return notesGlobal[octave][noteName][sign];
         });
 }
 
@@ -33,7 +42,6 @@ const notes = getNotes();
 
 const run = () => {
     let el = keyArray[notes.shift()];
-
     el.dispatchEvent(new Event('click'));
     if(notes.length)
         setTimeout(run, 100);
