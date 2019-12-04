727dd63ad0623cc60ca76035500af717dbcf94c3
aSosunoff <al.sosunoff@gmail.com>
2019-12-04 08:23:07 +0300

    2E fixed

--- 2018_front/2E/solution.js
+++ 2018_front/2E/solution.js
@@ -4,19 +4,19 @@ function getKeyboard(){
 
 function getNotes(){
     let buttons = {
-        "C1": 0, "D1 flat": 1,
-        "D1": 2, "E1 flat": 3,
+        "C1": 0, "C1 sharp": 1, "D1 flat": 1,
+        "D1": 2, "D1 sharp": 3, "E1 flat": 3,
         "E1": 4,
-        "F1": 5, "G1 flat": 6,
-        "G1": 7, "A1 flat": 8,
-        "A1": 9, "H1 flat": 10,
+        "F1": 5, "F1 sharp": 6, "G1 flat": 6,
+        "G1": 7, "G1 sharp": 8, "A1 flat": 8,
+        "A1": 9, "A1 sharp": 10, "H1 flat": 10,
         "H1": 11,
-        "C2": 12, "D2 flat": 13,
-        "D2": 14, "E2 flat": 15,
+        "C2": 12, "C2 sharp": 13, "D2 flat": 13,
+        "D2": 14, "D2 sharp": 15, "E2 flat": 15,
         "E2": 16,
-        "F2": 17, "G2 flat": 18,
-        "G2": 19, "A2 flat": 20,
-        "A2": 21, "H2 flat": 22,
+        "F2": 17, "F2 sharp": 18, "G2 flat": 18,
+        "G2": 19, "G2 sharp": 20, "A2 flat": 20,
+        "A2": 21, "A2 sharp": 22, "H2 flat": 22,
         "H2": 23
     };
 
@@ -39,6 +39,4 @@ const run = () => {
         setTimeout(run, 100);
 };
 
-window.onload = function() {
-    run();
-}
+run();
